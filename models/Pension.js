8e1103cade9ff7e36dac8b6978a7c9d099262969
Eliza Dascal <elizadascal@gmail.com>
2017-07-14 17:49:27 +0100

    Added models for PensionPage , TeamMenber & Layout for GA

--- models/Pension.js
+++ models/Pension.js
@@ -1,3 +1,25 @@
+var keystone = require('keystone');
+var Types = keystone.Field.Types;
+
 /**
- * Created by david on 14/07/2017.
+ * Pension Page Model
+ * ===========
  */
+
+var Pension = new keystone.List('Pension', {
+	track: true,
+	sortable: true,
+});
+
+Pension.add({
+	name: { type: String, required: true, initial: true },
+	isLightningTalk: { type: Boolean },
+	who: { type: Types.Relationship, ref: 'User', many: true, index: true },
+	description: { type: Types.Html, wysiwyg: true },
+	slides: { type: Types.Url },
+	link: { type: Types.Url }
+});
+
+
+Pension.defaultColumns = 'name, meetup|20%, who|20%';
+Pension.register();
--- models/TeamMember.js
+++ models/TeamMember.js
@@ -1,3 +1,26 @@
+var keystone = require('keystone');
+var Types = keystone.Field.Types;
+
 /**
- * Created by david on 14/07/2017.
+ * TeamMember Model
+ * ===========
  */
+
+var TeamMember = new keystone.List('TeamMember', {
+	track: true,
+	sortable: true,
+});
+
+TeamMember.add({
+	name: { type: String, required: true, initial: true },
+	isLightningTalk: { type: Boolean },
+	who: { type: Types.Relationship, ref: 'User', many: true, index: true },
+	description: { type: Types.Html, wysiwyg: true },
+	slides: { type: Types.Url },
+	link: { type: Types.Url },
+});
+
+
+TeamMember.defaultColumns = 'name';
+TeamMember.register();
+
