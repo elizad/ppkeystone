a546365bc22de12498d87bc805e60d9ff6fc35cc
eliza d <elizadascal@gmail.com>
2017-08-23 17:27:17 +0100

    UI + refactoring

--- routes/views/awardsandreviews.js
+++ routes/views/awardsandreviews.js
@@ -5,6 +5,12 @@ exports = module.exports = async function (req, res) {
 	var view = new keystone.View(req, res);
 	var locals = res.locals;
 	var awardsandreviews = {};	// assuming it should be an object
+	var calltoaction = {};
+	try {
+		calltoaction = await keystone.list('CallToAction').model.findOne().exec();
+	} catch (error) {
+		console.log('could not find ', error);
+	}
 	try {
 		awardsandreviews = await keystone.list('AwardsAndReviews').model.findOne().exec();
 	} catch (error) {
@@ -14,9 +20,11 @@ exports = module.exports = async function (req, res) {
 	locals.section = 'awards-and-reviews';
 	locals.filters = {
 		awardsandreviews: req.params.awardsandreviews,
+		calltoaction: req.params.calltoaction,
 	};
 	locals.data = {
 		awardsandreviews,
+		calltoaction,
 	};
 	// Load
 	view.on('init', function (next) {
